fc94075b714c396a2c949f923f8cdbb8ba46f5f4
huseyn9922 <68601129+huseyn9922@users.noreply.github.com>
2020-10-14 21:56:39 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,56 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.menu'),
+        close = document.querySelector('.menu__close');
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+// $(document).ready(function(){
+    // carousel
+        $('.center').slick({
+            centerMode: true,
+            centerPadding: '60px',
+            slidesToShow: 3,
+            speed: 500,
+            prevArrow:'<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+            // adaptiveHeight: true,
+            autoplay: true,
+            autoplaySpeed: 2000,                                  
+            responsive: [
+              {
+                breakpoint: 768,
+                settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 3
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 1
+                }
+              }
+            ]
+          });
+        // });
+        new WOW().init();
+
+        
